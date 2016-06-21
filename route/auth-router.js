305473b918dda2bf0406aa19c4a664e12454e5f7
Matthew Smith <mattsmith203@hotmail.com>
2016-06-20 20:10:09 -0700

    added signin route and changed signInRouter to authRouter

--- route/auth-router.js
+++ route/auth-router.js
@@ -4,12 +4,19 @@ const Router = require('express').Router;
 const jsonParser = require('body-parser').json();
 const debug = require('debug')('shooter:sigupRouter');
 const authController = require('../controller/auth-controller');
+const parseBasicAuth = require('../lib/parse-basic-auth');
 
-const signUpRoute = module.exports = new Router();
+const authRouter = module.exports = new Router();
 
-signUpRoute.post('/signup', jsonParser, function(req, res, next){
+authRouter.post('/signup', jsonParser, function(req, res, next){
   debug('post api/signup');
   authController.newUser(req.body)
   .then((token) => res.send(token))
+  .catch(next);
+});
+
+authRouter.get('/signin', parseBasicAuth, function(req, res, next){
+  authController.signIn(req.auth)
+  .then(token => res.send(token))
   .catch(next);
 });
