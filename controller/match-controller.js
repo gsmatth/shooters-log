71764034dd09a8e794e5069352fa4c64be97a532
Tuesday-Last <sgnortz@gmail>
2016-06-21 19:19:40 -0700

    created match-controller
    match controller contains post, fetch, delete, and delete-all operations

---
+++ controller/match-controller.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const debug = require('debug')('shooter:matchController');
+const Match = require('../model/match-model');
+const httpErrors = require('http-errors');
+
+exports.createMatch = function(reqBody){
+  debug('matchController: createMatch');
+  return new Promise((resolve, reject) => {
+    new Match(reqBody)
+    .save()
+    .then(match => resolve(match)))
+    .catch(err => reject(httpErrors(400, err.message)));
+  });
+};
+
+exports.fetchMatch = function(id){
+  debug('matchController: fetchMatch');
+  return new Promise((resolve, reject) => {
+    match.findOne(_id: id)
+    .then(match => resolve(match))
+    .catch(err => reject(httpErrors(404, err.message)));
+  });
+};
+
+exports.removeMatch = function(id){debug('matchController: fetchMatch');
+  debug('matchController: removeMatch')
+  return new Promise((resolve, reject) => {
+    match.findOne(_id: id)
+    .then(match => remove(match))
+    .then(resolve)
+    .catch(err => reject(httpErrors(404, err.message)));
+  });
+};
+
+exports.removeAllMatches = function(){
+  debug('matchController: removeAllMatches');
+  return Match.remove({});
+};
