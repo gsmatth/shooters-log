9a4ba917633766cc8e7ffb4ee5c996d5e33cac48
Doug Popadince <gsmatth@icloud.com>
2016-06-22 14:50:28 -0700

    added score.controller.js with createScore function expression

---
+++ controller/score-controller.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const debug = require('debug')('shooter:score-controller');
+const httpErrors = require('http-errors');
+const Score = require('../model/socre-model');
+
+exports.createScore = function(scoreData){
+  debug('entered createScore in score-controller');
+  return new Promise ((resolve, reject) => {
+    new Score(scoreData).save()
+    .then score => resolve (score)
+    .catch(err => reject(httpErrors(400, err.message)));
+  });
+};
