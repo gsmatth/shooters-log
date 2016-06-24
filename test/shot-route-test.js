3cbf9ddc6ae5b13075428519c8f74facd02c23b6
Doug Popadince <gsmatth@icloud.com>
2016-06-23 17:43:28 -0700

    added shot-router-test POST test
    *updated server.js by adding shot-router module and adding shotRouter as app.use
    *added POST test in shot-route-test module
    *debugged shot-model module
    	modified:   lib/parse-bearer-auth.js
    	modified:   model/shot-model.js
    	modified:   route/shot-router.js
    	modified:   server.js
    	new file:   test/shot-route-test.js

---
+++ test/shot-route-test.js
@@ -0,0 +1,187 @@
+'use strict';
+
+process.env.APP_SECRET = process.env.APP_SECRET || 'Im a secret!!!!!';
+process.env.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/test';
+
+// npm
+const expect = require('chai').expect;
+const request = require('superagent-use');
+const superPromise = require('superagent-promise-plugin');
+const debug = require('debug')('shooter:shot-route-test');
+
+const Shot = require('../model/shot-model');
+const authController = require('../controller/auth-controller');
+// const competitionController = require('../controller/competition-controller');
+const shotController = require('../controller/shot-controller');
+// const matchController = require('../controller/match-controller');
+
+const port = process.env.PORT || 3000;
+
+const baseUrl = `localhost:${port}/api`;
+const server = require('../server');
+request.use(superPromise);
+
+describe('Testing shot-route ', function() {
+
+  before((done)=> {
+    debug('before module shot-router');
+    if(!server.isRunning){
+      server.listen(port, () => {
+        server.isRunning = true;
+        debug(`server up on port ${port}`);
+        done();
+      });
+      return;
+    }
+    done();
+  });
+  after((done)=> {
+    debug('after module shot-router');
+    if(server.isRunning) {
+      server.close(()=>{
+        server.isRunning = false;
+        debug('server down');
+        done();
+      });
+      return;
+    }
+    done();
+  });
+
+  describe('testing POST shot route with valid  user, match, and competition', function(){
+
+    before((done) => { // create token for authorization needed for post
+      debug('shot-POST-route-test-before-block');
+      authController.newUser({username: user.username, password: user.password})
+      .then( token => {
+        this.tempToken = token;
+        done();
+      })
+      .catch(done);
+    });
+
+    after((done)=>{
+      debug('shot-POST-route-test-after-block');
+      Promise.all([
+        shotController.removeAllShots(),
+        authController.removeAllUsers()
+      ])
+      .then(() => done())
+      .catch(done);
+    });
+
+
+    var user = {
+      _id: '576c47d854d007350a734560',
+      password: '$2a$09$4zNSZ5AtttLPnjs8KaXpaur4aRucsAqesMqSLe0wt4fXL.X7fDb1C',
+      username: 'McTest',
+      findHash: 'f517531581cb0323dea580d7c0016a79812e7ffa3790f04786ee836d2fac1822'
+    };
+    var competition = {
+      _id: '576c4a4011d3f63f0a05d475',
+      userId: '576c47d854d007350a734560',
+      location: 'Ben Avery',
+      action: 'BAT'
+    };
+    var match = {
+      competitionId: '576c4a4011d3f63f0a05d475',
+      userId: '576c47d854d007350a734560',
+      matchNumber: 1,
+      _id: '576c4f19965f8a8a0ab5397f'
+    };
+    var shotData = {
+      userId:'576c47d854d007350a734560' ,
+      matchId: '576c4f19965f8a8a0ab5397f',
+      xValue: true,
+      score: '10'
+    };
+
+
+    it('should should return a new score object in response', (done) => {
+      debug('shot-POST-route-test-it-block');
+      request.post(`${baseUrl}/competition/${competition._id}/match/${match._id}/shot`)
+      .send(shotData)
+      .set({Authorization: `Bearer ${this.tempToken}`})
+      .then((res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.xValue).to.equal(true);
+        expect(res.body.score).to.equal('10');
+        console.log('\nSCORE RES BODY: \n \n', res.body);
+        done();
+      })
+      .catch(done);
+    });
+  });
+
+
+
+  describe('testing GET shot route with valid shotId', function(){
+
+    before((done) => { // create token for authorization needed for post
+      debug('shot-GET-route-test-before-block');
+      authController.newUser({username: user.username, password: user.password})
+      .then( token => {
+        this.tempToken = token;
+        console.log('\n GET TOKEN: \n'. this.tempToken);
+        shotController.createShot(shotData)
+      })
+        .then(shot => {
+            this.tempShot = shot;
+            console.log('\n TEMP SHOT: \n', this.tempShot);
+            done();
+        })
+          .catch(done);
+      });
+
+    after((done)=>{
+      debug('shot-GET-route-test-after-block');
+      Promise.all([
+        shotController.removeAllShots(),
+        authController.removeAllUsers()
+      ])
+      .then(() => done())
+      .catch(done);
+    });
+
+
+    var user = {
+      _id: '576c47d854d007350a734560',
+      password: '$2a$09$4zNSZ5AtttLPnjs8KaXpaur4aRucsAqesMqSLe0wt4fXL.X7fDb1C',
+      username: 'McTest',
+      findHash: 'f517531581cb0323dea580d7c0016a79812e7ffa3790f04786ee836d2fac1822'
+    };
+    var competition = {
+      _id: '576c4a4011d3f63f0a05d475',
+      userId: '576c47d854d007350a734560',
+      location: 'Ben Avery',
+      action: 'BAT'
+    };
+    var match = {
+      competitionId: '576c4a4011d3f63f0a05d475',
+      userId: '576c47d854d007350a734560',
+      matchNumber: 1,
+      _id: '576c4f19965f8a8a0ab5397f'
+    };
+    var shotData = {
+      userId:'576c47d854d007350a734560' ,
+      matchId: '576c4f19965f8a8a0ab5397f',
+      xValue: false,
+      score: 'M'
+    };
+
+
+    it('should should return an existing score object in response', (done) => {
+      debug('shot-GET-route-test-it-block');
+      request.get(`${baseUrl}/competition/${competition._id}/match/${match._id}/shot/${this.tempShot._id}`)
+      .set({Authorization: `Bearer ${this.tempToken}`})
+      .then((res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.xValue).to.equal(false);
+        expect(res.body.score).to.equal('M');
+        console.log('\nSCORE RES BODY: \n \n', res.body);
+        done();
+      })
+      .catch(done);
+    });
+  });
+});
