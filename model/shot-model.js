e126e7483fcfa6c2dc9d4e897c3e9a30a92ceafe
Doug Popadince <gsmatth@icloud.com>
2016-06-26 11:12:19 -0700

    added date property to shot model, route, test

--- model/shot-model.js
+++ model/shot-model.js
@@ -1,14 +1,13 @@
 'use strict';
 
 const mongoose = require('mongoose');
-// const debug = require('debug')('shooter:shot-model');
-// const httpErrors = require('http-errors');
 
 const shotSchema = module.exports = mongoose.Schema({
   userId: {type: mongoose.Schema.ObjectId, required:true},
   matchId: {type: mongoose.Schema.ObjectId, required:true},
   xValue: {type: Boolean, required: true},
-  score: {type: String, required: true}
+  score: {type: String, required: true},
+  dateOf: {type: String}
 });
 
 module.exports = mongoose.model('shot', shotSchema);
