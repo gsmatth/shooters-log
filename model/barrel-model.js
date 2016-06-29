8d73344198497980fa3fa6560f1c204d6bea6f80
Doug Popadince <gsmatth@icloud.com>
2016-06-28 19:22:31 -0700

    added barrel model

---
+++ model/barrel-model.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const barrelSchema = module.exports = mongoose.Schema({
+
+  barrelManufacturer: {type: String},
+  barrelType: {type: String},
+  barrelTwist: {type: String},
+  barrelLength: {type: Number},
+  barrelLife: {type: Number},
+  barrelCaliber: {type: Number},
+  roundCount: {type: Number},
+  matchId:{type: mongoose.Schema.ObjectId},
+  competitionId:{type: mongoose.Schema.ObjectId},
+  userId:{type: mongoose.Schema.ObjectId, required: true},
+  rifleId: {type: mongoose.Schema.ObjectId}
+});
+
+
+module.exports = mongoose.model('barrel', barrelSchema);
