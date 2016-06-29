5f6454784355026cfdb361fc528efa8f580399e9
Doug Popadince <gsmatth@icloud.com>
2016-06-28 19:37:45 -0700

    added barrel-router skeleton

--- route/barrel-router.js
+++ route/barrel-router.js
@@ -1,2 +1,28 @@
 'use strict';
-const Router
+
+
+const Router = require('express').Router;
+const debug = require('debug')('shooter:barrel-router');
+const parseBearerAuth = require('../lib/parse-bearer-auth');
+const barrelRouter = module.exports = new Router();
+const jsonParser = require('body-parser').json();
+const httpErrors = require('http-errors');
+
+
+
+barrelRouter.get('/user/:userid/barrel/:barrelid', parseBearerAuth, function(req, res, next){
+  debug('entered barrel.get route');
+
+});
+
+barrelRouter.post('/user/:userid/barrel', parseBearerAuth, function(req, res, next){
+  debug('entered barrel.post route');
+});
+
+barrelRouter.put('/user/:userid/barrel/:barrelid', parseBearerAuth, function(req, res, next){
+  debug('entered barrel.put route');
+});
+
+barrelRouter.delete('/user/:userid/barrel/:barrelid', parseBearerAuth, function(req, res, next){
+  debug('entered barrel.delete route');
+});
